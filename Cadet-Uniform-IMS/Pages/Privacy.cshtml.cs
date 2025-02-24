96adfcc214f579846d71b7024c3d3926106caf6e
Harry <harry.sumner@outlook.com>
2025-02-24 22:29:38 +0000

    - Fixing issue with rebase.

---
+++ Cadet-Uniform-IMS/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cadet_Uniform_IMS.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+

2a3df65bfeb7eeea2a6a931201fae30130efb2f4
Harry <harry.sumner@outlook.com>
2025-02-24 22:31:22 +0000

    - Fixing git issue

--- Cadet-Uniform-IMS/obj/Debug/net8.0/Cadet-Uniform-IMS.AssemblyInfo.cs
+++ Cadet-Uniform-IMS/obj/Debug/net8.0/Cadet-Uniform-IMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cadet-Uniform-IMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49af08ac485849b3da924b146f7b950c01bf2b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96adfcc214f579846d71b7024c3d3926106caf6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cadet-Uniform-IMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cadet-Uniform-IMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
